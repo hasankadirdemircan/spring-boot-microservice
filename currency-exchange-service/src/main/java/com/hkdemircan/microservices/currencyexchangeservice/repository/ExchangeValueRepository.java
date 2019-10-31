5a8409061597a1e5f506aae49d19fbaf537a6ac4
Hasan Kadir Demircan <hkdemircan06@gmail.com>
2019-10-31 22:30:52 +0300

    rollback microservice

--- currency-exchange-service/src/main/java/com/hkdemircan/microservices/currencyexchangeservice/repository/ExchangeValueRepository.java
+++
@@ -1,9 +0,0 @@
-package com.hkdemircan.microservices.currencyexchangeservice.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.hkdemircan.microservices.currencyexchangeservice.model.ExchangeValue;
-
-public interface ExchangeValueRepository extends JpaRepository<ExchangeValue, Long> {
-	ExchangeValue findByFromAndTo(String from, String to);
-}
